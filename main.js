e7a03a7904b7b2e0ffba0bab2b4311265e84606d
MikkeBee <mjblakley87@gmail.com>
2022-03-10 14:55:51 +0200

    pokemon project almost complete

---
+++ main.js
@@ -0,0 +1,43 @@
+const gridCont = document.querySelector(".gridContainer");
+
+const fetchData = () => {
+  fetch("https://pokeapi.co/api/v2/pokemon?limit=50&offset=0")
+    .then((res) => res.json())
+    .then((data) => {
+      //   console.log("fetch result", data);
+      //     pokeCard(data.results);
+      const fetches = data.results.map((item) => {
+        return fetch(item.url).then((res) => res.json());
+      });
+      Promise.all(fetches).then((res) => {
+        console.log(res);
+        pokeCard(res);
+      });
+    });
+};
+
+// Sprites.other.dream_world.front_default;
+
+const pokeCard = (data) => {
+  //   console.log(data);
+  //   console.log("pokecards and stuff", data);
+
+  const cards = data
+    .map((card) => {
+      return `<div class="cardSet">
+         <div class="card"><img src="${
+           card.sprites.other.dream_world.front_default
+         }" alt="${card.name}"></div>
+         <p class="cardText">${card.name}</p>
+         <p class="type">${card.types
+           .map((pokeType) => pokeType.type.name)
+           .join(", ")}
+            </p>
+       </div>`;
+    })
+    .join("");
+
+  gridCont.innerHTML = cards;
+};
+
+fetchData();
